2db69d38377bcb97695731b6f9c59ff5e22dd33e
= <=>
2021-10-14 22:04:53 -0500

    functional db querying w chart population

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import Charts from './Chart'
 import { LineChart, XAxis, YAxis, Tooltip, CartesianGrid, Line } from 'recharts';
-import {useState, useEffect} from 'react'
-const data = [
-  { time: '21:20', TCP: 100, UDP: 100, ICMP: 400 },
-  { time: '21:30', TCP: 1000, UDP: 2400, ICMP: 2400 },
-  { time: '21:40', TCP: 1000, UDP: 2400, ICMP: 2400 },
-];
-function App() {
-  
-  useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/packetCount')
-      const data = await res.json()
 
-      console.log(data)
-    }
+import {React, ReactDOM, useState, useEffect} from 'react'
+// const data = [
+//   { time: '21:20', TCP: 100, UDP: 100, ICMP: 400 },
+//   { time: '21:30', TCP: 1000, UDP: 2400, ICMP: 2400 },
+//   { time: '21:40', TCP: 1000, UDP: 2400, ICMP: 2400 },
+// ];
+function App() {
+  const [stats, setStats] = useState([
+    { time: '21:40', TCP: 1000, UDP: 2400, ICMP: 2400 }
+  ])
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/packet_stats")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true)
+          setStats(result)
+        },
+        (error) => {
+          setIsLoaded(true)
+          setError(error)
+        }
+      )
   }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-      <LineChart width={500} height={300} data={data}>
-      <XAxis dataKey="time"/>
-      <Line type="monotone" dataKey="ICMP" stroke="#387908" yAxisId={2} />
-      <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
-      <Line type="monotone" dataKey="TCP" stroke="#8884d8" />
-      <Line type="monotone" dataKey="UDP" stroke="#82ca9d" />
-    </LineChart>
-      
-      </header>
-    </div>
-  );
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  else {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <LineChart width={500} height={300} data={stats}>
+        <XAxis dataKey="time"/>
+        <Line type="monotone" dataKey="ICMP" stroke="#387908" yAxisId={2} />
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+        <Line type="monotone" dataKey="TCP" stroke="#8884d8" />
+        <Line type="monotone" dataKey="UDP" stroke="#82ca9d" />
+        </LineChart>
+
+        
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
